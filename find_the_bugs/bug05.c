db408ce734d3ba0d86c0b07549a28727248c41a1
Mark Ellzey <mark.thomas@mandiant.com>
2010-10-06 14:56:35 -0400

    Making bug05.c harder

--- find_the_bugs/bug05.c
+++ find_the_bugs/bug05.c
@@ -7,16 +7,18 @@
 #include <stdint.h>
 #include <errno.h>
 
-#define inline_loop(string)              \
-    int i;                               \
-    const char *ptr = string;            \
-    for (i = 0; i < *ptr != '\0'; i++) { \
-        printf("%c\n", *ptr++);          \
-    }
+#define inline_loop(string)                  \
+        int         i;                       \
+        const char *ptr = string;            \
+        for (i = 0; i < *ptr != '\0'; i++) { \
+            printf("%c\n", *ptr++);          \
+        }
 
 int main(int argc, char **argv) {
-    if (argc > 2) 
+    if (argc > 2)
         inline_loop(argv[1]);
+    else
+        printf("no arguments..\n");
 
     return 0;
 }
