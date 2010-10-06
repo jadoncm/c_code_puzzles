4e08a6b76a86b5628072269ef828a1fbfc4c170c
Mark Ellzey <mark.thomas@mandiant.com>
2010-10-06 11:57:13 -0400

    Added bug05.c

---
+++ find_the_bugs/bug05.c
@@ -0,0 +1,24 @@
+/* Why does this code not compile? What is the fix? (There are two ways, but
+ * only one is optimal) */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <errno.h>
+
+#define inline_loop(string)              \
+    int i;                               \
+    const char *ptr = string;            \
+    for (i = 0; i < *ptr != '\0'; i++) { \
+        printf("%c\n", *ptr++);          \
+    }
+
+int main(int argc, char **argv) {
+    if (argc > 2) {
+        inline_loop(argv[1]);
+    }
+
+    return 0;
+}
+
