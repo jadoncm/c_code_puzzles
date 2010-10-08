3964499780746b5eeec5dd11ede407178425e011
Mark Ellzey <mark.thomas@mandiant.com>
2010-10-08 12:03:50 -0400

    adding bug08

---
+++ find_the_bugs/bug08.c
@@ -0,0 +1,32 @@
+/* something here is sinister, figure it out! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <errno.h>
+
+int main(int argc, char **argv) {
+    char *input;
+    char *output;
+    int i;
+
+    if (argc < 2) {
+	fprintf(stderr, "Usage: %s <string>\n", argv[0]);
+	exit(1);
+    }
+
+    input  = malloc(strlen(argv[1]));
+    output = malloc(strlen(argv[1]));
+
+    for (i = 0; i <= strlen(argv[1]); i++) {
+	input[i] = argv[1][i];
+    }
+
+    output = strdup(input);
+
+    free(input);
+    free(output);
+
+    return 0;
+}
+
