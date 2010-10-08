9451db498f76ab510c9ca02624ef73c93a1aaff7
Mark Ellzey <mark.thomas@mandiant.com>
2010-10-08 11:51:28 -0400

    Adding bug07

---
+++ find_the_bugs/bug07.c
@@ -0,0 +1,27 @@
+/* why does this program cause a segfault? */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <errno.h>
+
+char *
+give_me_data(void) {
+    char *data;
+
+    data = alloca(1024);
+
+    strncpy(data, "hello, world", 1024);
+
+    return data;
+}
+
+int main(int argc, char **argv) {
+    char *data = give_me_data();
+
+    printf("%s\n", data);
+    free(data);
+
+    return 0;
+}
+
